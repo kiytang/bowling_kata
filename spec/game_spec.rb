04e127e36e06c82d055f184a62e2a4c92cd00847
Kiy Hong Tang <kiyhong@gmail.com>
2014-06-16 11:22:00 +0100

    Refactored to avoid duplication

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,17 +1,23 @@
 require_relative "../lib/game"
 
 #write a failing unit test
-#initialize a new object of Bike class 
+#initialize a new object of Game class 
 describe Game do
+
+  #initilaize in setup method, which creates game
+  let(:game) {Game.new}
+
   it "can create a new game" do
-    game = Game.new
+    expect(game).to be_true
+  end
+
+
+  #write a role method
+  # forgot to place a do after it
+  context "#roll" do
+    it "can roll" do
+      game.roll(0)
+    end
   end
 end
 
-#write a role method
-context "#{}roll" do
-  "it can roll"
-    game = Game.new
-    game.roll(0)
-  end
-end
