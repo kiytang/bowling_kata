534e093d6808d422dfa7de6ca6b977c7a667132c
Kiy Hong Tang <kiyhong@gmail.com>
2014-06-16 17:13:34 +0100

    Refactored method score

--- lib/game.rb
+++ lib/game.rb
@@ -10,25 +10,28 @@ class Game
   end
 
   # Know that our test below works as we have tested two cases:
-  # initially I placed return -1, this caused the test to 
+  # initially first_in_frame placed return -1, this caused the test to 
   # fail, then inputting return 0, passes test.
   def score
     score = 0
     frame = 0
-    i = 0
+    first_in_frame = 0  #index of the first ball in the frame
     while frame < 10 # loop through frame 1 at a time
-      if @rolls[i] + @rolls[i+1] == 10 #spare
-        score += 10 + @rolls[i+2] #
-        i += 2
+      if is_spare?(first_in_frame) #spare
+        score += 10 + @rolls[first_in_frame+2] #score for spare is 10 + the next ball you roll
+        first_in_frame += 2 #x2 balls in this frame thus increment first_in_frame by 2:walking through the array x2 balls at a time
       else
-        score += @rolls[i] + @rolls[i+1]
-        i += 2
+        score += @rolls[first_in_frame] + @rolls[first_in_frame+1]
+        first_in_frame += 2
       end
       frame += 1
     end  
     score
   end
 
+  def is_spare?(first_in_frame)
+    @rolls[first_in_frame] + @rolls[first_in_frame+1] == 10
+  end
 
 end
 
@@ -39,5 +42,5 @@ end
 # inside of roll then : score = score + pins
 # inside of score return score
 
-# here I will use initialize method within the game class 
+# here first_in_frame will use initialize method within the game class 
 # and set it to 0 
