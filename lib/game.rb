6d4d0f7536c5d3d1c8160ac4e74dc5582b796be1
Kiy Hong Tang <kiyhong@gmail.com>
2014-06-17 10:28:10 +0100

    Refactored and removed comments

--- lib/game.rb
+++ lib/game.rb
@@ -1,28 +1,25 @@
-#game has role and score method 
 class Game
 
   def initialize
-    @rolls = []  #hold our rolls
+    @rolls = []
   end
 
   def roll(pins)
     @rolls << pins
   end
 
-  # Know that our test below works as we have tested two cases:
-  # initially first_in_frame placed return -1, this caused the test to 
-  # fail, then inputting return 0, passes test.
   def score
     score = 0
     frame = 0
-    first_in_frame = 0  #index of the first ball in the frame
-    while frame < 10 # loop through frame 1 at a time
+    first_in_frame = 0  
+    
+    while frame < 10
       if is_strike?(first_in_frame) 
         score += 10 + next_two_balls_for_strike(first_in_frame)
-        first_in_frame +=1 #only x1 ball in stike frame
+        first_in_frame +=1 
       elsif is_spare?(first_in_frame)
         score += 10 + next_ball_for_spare(first_in_frame)
-        first_in_frame += 2 #x2 balls in this frame thus increment first_in_frame by 2:walking through the array x2 balls at a time
+        first_in_frame += 2 
       else
         score += two_balls_in_frame(first_in_frame)
         first_in_frame += 2
@@ -32,8 +29,10 @@ class Game
     score
   end
 
+  private
+  
   def is_spare?(first_in_frame)
-    @rolls[first_in_frame] + @rolls[first_in_frame+1] == 10
+    two_balls_in_frame(first_in_frame) == 10
   end
 
   def is_strike?(first_in_frame)
@@ -45,20 +44,10 @@ class Game
   end
 
   def next_ball_for_spare(first_in_frame)
-    @rolls[first_in_frame+2] #score for spare is 10 + the next ball you roll
+    @rolls[first_in_frame+2] 
   end
 
   def two_balls_in_frame(first_in_frame)
     @rolls[first_in_frame] + @rolls[first_in_frame+1]
   end
 end
-
-
-
-# #add up all the pins in roll
-# inside of pins we can initilaize score = 0
-# inside of roll then : score = score + pins
-# inside of score return score
-
-# here first_in_frame will use initialize method within the game class 
-# and set it to 0 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,20 +1,9 @@
 require_relative "../lib/game"
 
-#write a failing unit test
-#initialize a new object of Game class 
 describe Game do
 
-  #initilaize in setup method, which creates game
   let(:game) {Game.new}
 
-  #Refactor phase
-# extract a function that contains the block, call 
-# it the function roll_many.
-# take 20 and turn it to a var called n and take 0 and 
-# turn it into  a var called pins
-# then take the var out of block, (perametric)
-# and extract the method rollmany it will take n and pin as
-# arguments
   def roll_many(n, pins)
     n.times do
       game.roll(pins)
@@ -26,52 +15,43 @@ describe Game do
     game.roll(5) 
   end
 
-  it "can create a new game" do
-    # expect(game).to be_true
+  def roll_strike
+    game.roll(10)
   end
 
-
-  #write a role method
-  # forgot to place a do after it
   context "#roll" do
     it "can roll" do
       game.roll(0)
     end
   end
 
-  # for score method as it is based on the total of the 
-  # 10 frames, we will use the most degenerative complete game 
-  # we can roll is a gutter game (20 balls in a gutter game)
-
   it "returns 0 for a complete gutter game" do
     roll_many(20,0)
     expect(game.score).to eq 0
   end
 
-  # next most trivial game is based on a score of all ones
-  # thus returning a total score of 20
   it "returns 20 for a complete game where 1 is recorded for every turn" do
     roll_many(20, 1)
     expect(game.score).to eq 20
   end
 
-  #next most interesting test case:
-  # all 5 won't pass as they mean all spares
-  # The simplest spare test is x1 spare followed by gutter balls
-
   it "records for spares" do
     roll_spare
     game.roll(3)  
-    roll_many(17,0)#x3 turns already taken, thus 17 remaining   
-    expect(game.score).to eq 16 #3 counted twice for spare
+    roll_many(17,0)
+    expect(game.score).to eq 16
   end
 
   it "records for stikes" do
-    game.roll(10) #strike
+    roll_strike
     game.roll(3)
     game.roll(4)
-    roll_many(16,0) #8 frames of gutter balls
+    roll_many(16,0)
     expect(game.score).to eq 24
   end
-end
 
+  it "rolls a perfect game" do  
+    roll_many(12, 10)
+    expect(game.score).to eq 300
+  end
+end
